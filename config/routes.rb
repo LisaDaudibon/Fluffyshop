292ba03b137db15bcd0db4074a63b58ab2cdfd41
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-13 18:58:29 +0100

    route home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ 
+  root "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
