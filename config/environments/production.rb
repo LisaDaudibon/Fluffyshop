08f97b20a82764c296a684cb29d8ef4bb9f2f2ee
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-14 17:04:05 +0100

    test branchement mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -63,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Fluffyshop_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'fluffyshop.herokuapp.com' }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
