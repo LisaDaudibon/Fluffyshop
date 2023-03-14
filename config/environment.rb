0456bae2c4d046f561d5fb8ad6f094a882ef490a
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-14 17:26:52 +0100

    test

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
+  :domain => 'https://fluffyshop.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
