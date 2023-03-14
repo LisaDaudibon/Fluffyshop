950d958a650d6ec66cbf7f290c33f6ce1f003f37
Suziwan <christe.suzanne@gmail.com>
2023-03-14 11:01:32 +0000

    Fix bug password edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 end
