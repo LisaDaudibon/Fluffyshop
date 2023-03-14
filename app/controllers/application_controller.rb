9e0894caa845609053e34957abe0dd2ee23f3307
Suziwan <christe.suzanne@gmail.com>
2023-03-14 10:26:41 +0000

    Fix find names in devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+  end
 end
