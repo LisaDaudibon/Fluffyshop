dea791619e7b50bf729f32d48ef4fcf2e64ecce0
Chandara_LOEK <chandara.loek@outlook.com>
2023-03-14 19:28:32 +1100

    second version seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,6 @@ ActiveRecord::Base.connection.reset_pk_sequence!('items')
 end 
 
 picture_item = ['photo chat mignon', 'photo chat noir','photo chat blanc', 'photo chat gouttière','photo chat roux','photo chat tigré']
-object_item = ['mug','poster','tableau','tablier', 'ensemble de lit', 'bouteille', 'assiette', 'coque de smartphone', 'totbag', 'porte-clé', 'serviette', 'set de table']
 description_picture = [
 	"La photo de chat montre un félin à la fourrure épaisse et soyeuse, avec des rayures distinctives qui lui donnent un air sauvage.",
 
@@ -48,13 +47,31 @@ url_picture = [
 		)
 end
 
-14.times do |i|
-	Item.create(
-		name: object_item.sample,
-		description: "l'objet parfait à customiser",
-		price: Faker::Number.between(from: 1.00, to: 30.00).round(2),
-		image_url: url_picture.sample
-		)
-end
+
+Item.create(
+	name: 'mug',
+	description: "l'objet parfait à customiser",
+	price: Faker::Number.between(from: 1.00, to: 30.00).round(2),
+	image_url: "https://www.lapetiteboutiqueaclic.com/wp-content/uploads/2018/06/mug-chat-01-recto.png",
+	)
+
+Item.create(
+	name: 'sticker',
+	description: "l'objet parfait à customiser",
+	price: Faker::Number.between(from: 1.00, to: 30.00).round(2),
+	image_url: "https://m.media-amazon.com/images/I/61FFpPQgVeL._SR600%2c315_PIWhiteStrip%2cBottomLeft%2c0%2c35_PIStarRatingFOURANDHALF%2cBottomLeft%2c360%2c-6_SR600%2c315_ZA347%2c445%2c290%2c400%2c400%2cAmazonEmberBold%2c12%2c4%2c0%2c0%2c5_SCLZZZZZZZ_FMpng_BG255%2c255%2c255.jpg",
+	)
+
+Item.create(
+	name: 'magnet',
+	description: "l'objet parfait à customiser",
+	price: Faker::Number.between(from: 1.00, to: 30.00).round(2),
+	image_url: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/812WZ6nMR+L._AC_UL320_.jpg",
+	)
+
+
+
+
+
 
 
