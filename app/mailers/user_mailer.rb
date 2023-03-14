52321832d3c81f6f162d37e71aeab4d5d5435985
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-14 17:37:15 +0100

    User_mailer update

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,16 @@
+class UserMailer < ApplicationMailer
+
+    default from: 'no-reply@fluffyshop.fr'
+
+    def welcome_email(user)
+      #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+      @user = user 
+  
+      #on définit une variable @url qu'on utilisera dans la view d’e-mail
+      @url  = 'https://fluffyshop.herokuapp.com' 
+  
+      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+      mail(to: @user.email, subject: 'Bienvenue chez nous !')
+    end
+
+end
