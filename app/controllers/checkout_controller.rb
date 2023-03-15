8ec32dd67e1848527a6f34160840b58b0c14cd28
Chandara_LOEK <chandara.loek@outlook.com>
2023-03-16 06:55:05 +1100

    checkout + views

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,32 @@
+class CheckoutController < ApplicationController
+  def create
+    @total = params[:total].to_d
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'eur',
+            unit_amount: (@total*100).to_i,
+            product_data: {
+              name: 'Rails Stripe Checkout',
+            },
+          },
+          quantity: 1
+        },
+      ],
+      mode: 'payment',
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url
+    )
+    redirect_to @session.url, allow_other_host: true
+  end
+
+  def success
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+  end
+
+  def cancel
+  end
+end
