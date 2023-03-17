d5df5f8da56adaf257afd94b836b24da42a82067
Chandara_LOEK <chandara.loek@outlook.com>
2023-03-17 20:31:39 +1100

    CL first commit past orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,13 @@
 class OrdersController < ApplicationController
 
   def index 
+    @orders = current_user.orders
+    @order = @orders.each do |order|
+    end 
+
+    order_item = @orders.each do |item|
+      item = @order.item
+    end 
   end 
 
   def create
